c5005e283463303684b9911f9c9d4be36986231e
coob <koosanghwi@gmail.com>
2024-04-25 14:43:11 +0900

    ver 1.03

--- data_arthdal/mymodule/tutorial.py
+++ data_arthdal/mymodule/tutorial.py
@@ -612,6 +612,16 @@ def tuto_imgs_scan(cla):
             pyautogui.keyUp('d')
             time.sleep(0.3)
 
+        # 82 이동하기
+        full_path = "c:\\my_games\\arthdal\\data_arthdal\\imgs\\tutorial\\82_move.PNG"
+        img_array = np.fromfile(full_path, np.uint8)
+        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        imgs_ = imgs_set_(200, 300, 800, 700, cla, img, 0.8)
+        if imgs_ is not None and imgs_ != False:
+            print("82_move", imgs_)
+
+            click_pos_reg(imgs_.x, imgs_.y, cla)
+
     except Exception as e:
         print(e)
         return 0
