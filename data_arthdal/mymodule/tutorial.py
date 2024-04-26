3ced3f6555982cf8fbe8a465201214299074b297
coob <koosanghwi@gmail.com>
2024-04-26 17:50:16 +0900

    ver 1.11

--- data_arthdal/mymodule/tutorial.py
+++ data_arthdal/mymodule/tutorial.py
@@ -655,6 +655,15 @@ def tuto_imgs_scan(cla):
             print("82_move", imgs_)
 
             click_pos_reg(imgs_.x, imgs_.y, cla)
+        else:
+            full_path = "c:\\my_games\\arthdal\\data_arthdal\\imgs\\jadong\\82_move_title_confirm.PNG"
+            img_array = np.fromfile(full_path, np.uint8)
+            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+            imgs_ = imgs_set_(200, 400, 800, 900, cla, img, 0.8)
+            if imgs_ is not None and imgs_ != False:
+                print("82_move_title_confirm", imgs_)
+
+                click_pos_reg(imgs_.x, imgs_.y, cla)
 
     except Exception as e:
         print(e)
