af0ba9f581c13291b1e53293666e51fdefe31af1
coob <koosanghwi@gmail.com>
2024-04-29 22:54:55 +0900

    ver 1.221

--- data_arthdal/mymodule/test_.py
+++ data_arthdal/mymodule/test_.py
@@ -46,7 +46,7 @@ def go_test():
         full_path = "c:\\my_games\\arthdal\\data_arthdal\\imgs\\dungeon\\time_zero.PNG"
         img_array = np.fromfile(full_path, np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        imgs_ = imgs_set_(360, 940, 360 + 80, 980, cla, img, 0.95)
+        imgs_ = imgs_set_(360, 940, 360 + 80, 980, cla, img, 1)
         if imgs_ is not None and imgs_ != False:
             print("sdfdskjfa")
         else:
