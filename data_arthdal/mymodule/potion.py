e346f77a6d3ff05a8fa4d256e6a9f84034e6da5f
coob <koosanghwi@gmail.com>
2024-04-30 04:27:08 +0900

    ver 1.341

--- data_arthdal/mymodule/potion.py
+++ data_arthdal/mymodule/potion.py
@@ -29,7 +29,7 @@ def potion_check(cla):
                 full_path = "c:\\my_games\\arthdal\\data_arthdal\\imgs\\potion\\juljun_num\\" + str(i) + ".PNG"
                 img_array = np.fromfile(full_path, np.uint8)
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                imgs_ = imgs_set_(422, 995, 434, 1020, cla, img, 0.9)
+                imgs_ = imgs_set_(425, 995, 435, 1020, cla, img, 0.9)
                 if imgs_ is not None and imgs_ != False:
                     print("i", i, imgs_)
                     need_potion = False
@@ -43,7 +43,7 @@ def potion_check(cla):
                     full_path = "c:\\my_games\\arthdal\\data_arthdal\\imgs\\potion\\out_num\\" + str(i) + ".PNG"
                     img_array = np.fromfile(full_path, np.uint8)
                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                    imgs_ = imgs_set_(664, 995, 678, 1020, cla, img, 0.9)
+                    imgs_ = imgs_set_(666, 995, 678, 1020, cla, img, 0.9)
                     if imgs_ is not None and imgs_ != False:
                         print("i", i, imgs_)
                         need_potion = False
