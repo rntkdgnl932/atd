29fd692e7896baa8f3d438863e869ea104d25b79
coob <koosanghwi@gmail.com>
2024-04-30 05:25:06 +0900

    ver 1.342

--- data_arthdal/mymodule/boonhae.py
+++ data_arthdal/mymodule/boonhae.py
@@ -122,6 +122,13 @@ def boonhae_ready(cla):
             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
             imgs_ = imgs_set_(30, 30, 120, 80, cla, img, 0.8)
             if imgs_ is not None and imgs_ != False:
+                full_path = "c:\\my_games\\arthdal\\data_arthdal\\imgs\\boonhae\\boonhae_confirm.PNG"
+                img_array = np.fromfile(full_path, np.uint8)
+                img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                imgs_ = imgs_set_(480, 570, 630, 630, cla, img, 0.8)
+                if imgs_ is not None and imgs_ != False:
+                    click_pos_reg(imgs_.x, imgs_.y, cla)
+
                 cleen_screen_start(cla)
             else:
                 break
