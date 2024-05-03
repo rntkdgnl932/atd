2aed640647dba6d6e875761c85941c90122a6e4c
coob <koosanghwi@gmail.com>
2024-05-03 14:16:58 +0900

    ver 1.424

--- data_arthdal/mymodule/dungeon.py
+++ data_arthdal/mymodule/dungeon.py
@@ -63,8 +63,8 @@ def dungeon_start(cla, sche):
                     print("대기중")
                     v_.dungeon_ready_count += 1
                     if v_.dungeon_ready_count > 1:
-                        why = "던전 진입 제대로 못해서 마을로 간다. 참고해라"
-                        line_to_me(cla, why)
+                        # why = "던전 진입 제대로 못해서 마을로 간다. 참고해라"
+                        # line_to_me(cla, why)
                         go_maul(cla)
                         dungeon_in(cla, sche)
                     else:
