ab6de5e4ac8a50405e3b08804dc1d98a056095e5
coob <koosanghwi@gmail.com>
2024-05-04 16:24:53 +0900

    ver 1.441

--- data_arthdal/mymodule/action_arthdal.py
+++ data_arthdal/mymodule/action_arthdal.py
@@ -439,37 +439,28 @@ def go_maul(cla):
             imgs_ = imgs_set_(50, 100, 110, 160, cla, img, 0.8)
             if imgs_ is not None and imgs_ != False:
 
-
-                full_path = "c:\\my_games\\arthdal\\data_arthdal\\imgs\\action\\maul\\jabhwa.PNG"
+                full_path = "c:\\my_games\\arthdal\\data_arthdal\\imgs\\chango\\chango_btn.PNG"
                 img_array = np.fromfile(full_path, np.uint8)
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                imgs_ = imgs_set_(60, 120, 160, 160, cla, img, 0.8)
+                imgs_ = imgs_set_(60, 150, 160, 180, cla, img, 0.8)
                 if imgs_ is not None and imgs_ != False:
                     is_maul = True
                     break
                 else:
                     drag_pos(120, 150, 120, 240, cla)
             else:
-                full_path = "c:\\my_games\\arthdal\\data_arthdal\\imgs\\potion\\jabhwa_sangin.PNG"
+                full_path = "c:\\my_games\\arthdal\\data_arthdal\\imgs\\action\\maul\\party_create.PNG"
                 img_array = np.fromfile(full_path, np.uint8)
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                imgs_ = imgs_set_(30, 30, 130, 80, cla, img, 0.8)
+                imgs_ = imgs_set_(70, 130, 150, 180, cla, img, 0.8)
                 if imgs_ is not None and imgs_ != False:
-                    is_maul = True
                     break
                 else:
-                    full_path = "c:\\my_games\\arthdal\\data_arthdal\\imgs\\action\\maul\\party_create.PNG"
-                    img_array = np.fromfile(full_path, np.uint8)
-                    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                    imgs_ = imgs_set_(70, 130, 150, 180, cla, img, 0.8)
-                    if imgs_ is not None and imgs_ != False:
-                        break
+                    result_out = out_check(cla)
+                    if result_out == True:
+                        click_pos_2(35, 160, cla)
                     else:
-                        result_out = out_check(cla)
-                        if result_out == True:
-                            click_pos_2(35, 160, cla)
-                        else:
-                            cleen_screen_start(cla)
+                        cleen_screen_start(cla)
             time.sleep(1)
         # 위에는 마을인지 파악하기
 
@@ -495,10 +486,10 @@ def go_maul(cla):
                     # result_out = out_check(cla)
                     # if result_out == True:
 
-                    full_path = "c:\\my_games\\arthdal\\data_arthdal\\imgs\\action\\maul\\jabhwa.PNG"
+                    full_path = "c:\\my_games\\arthdal\\data_arthdal\\imgs\\chango\\chango_btn.PNG"
                     img_array = np.fromfile(full_path, np.uint8)
                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                    imgs_ = imgs_set_(60, 120, 160, 160, cla, img, 0.8)
+                    imgs_ = imgs_set_(60, 150, 160, 180, cla, img, 0.8)
                     if imgs_ is not None and imgs_ != False:
                         break
                     else:
@@ -547,10 +538,10 @@ def go_maul(cla):
                         if imgs_ is not None and imgs_ != False:
                             click_pos_reg(imgs_.x, imgs_.y, cla)
                         else:
-                            full_path = "c:\\my_games\\arthdal\\data_arthdal\\imgs\\action\\maul\\jabhwa.PNG"
+                            full_path = "c:\\my_games\\arthdal\\data_arthdal\\imgs\\chango\\chango_btn.PNG"
                             img_array = np.fromfile(full_path, np.uint8)
                             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                            imgs_ = imgs_set_(60, 120, 160, 160, cla, img, 0.8)
+                            imgs_ = imgs_set_(60, 150, 160, 180, cla, img, 0.8)
                             if imgs_ is not None and imgs_ != False:
                                 break
                     time.sleep(1)
