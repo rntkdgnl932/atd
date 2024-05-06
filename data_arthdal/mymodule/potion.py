02f7e0879ae169519312e4751d6d5880922d616c
coob <koosanghwi@gmail.com>
2024-05-07 04:27:49 +0900

    ver 1.472

--- data_arthdal/mymodule/potion.py
+++ data_arthdal/mymodule/potion.py
@@ -46,7 +46,7 @@ def potion_check(cla):
                 full_path = "c:\\my_games\\arthdal\\data_arthdal\\imgs\\potion\\kind_potion\\" + str(kind_potion) + ".PNG"
                 img_array = np.fromfile(full_path, np.uint8)
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                imgs_ = imgs_set_(418, 995, 430, 1020, cla, img, 0.9)
+                imgs_ = imgs_set_(630, 970, 700, 1020, cla, img, 0.9)
                 if imgs_ is not None and imgs_ != False:
                     print("포션종류", i + 1, imgs_)
                     view_potion = True
