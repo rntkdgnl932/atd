1f838b76cf86c7146f8e89bcfa84d5f64b51fcfe
coob <koosanghwi@gmail.com>
2024-08-24 18:11:32 +0900

    ver 1.8301

--- data_arthdal/mymodule/chango.py
+++ data_arthdal/mymodule/chango.py
@@ -258,7 +258,7 @@ def chango_ready(cla):
                                             full_path = "c:\\my_games\\arthdal\\data_arthdal\\imgs\\chango\\box_list\\ganghwasuk_box_high.PNG"
                                             img_array = np.fromfile(full_path, np.uint8)
                                             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                                            imgs_ = imgs_set_(720, 120, 950, 990, cla, img, 0.8)
+                                            imgs_ = imgs_set_(720, 120, 950, 990, cla, img, 0.7)
                                             if imgs_ is not None and imgs_ != False:
                                                 print("ganghwasuk_box_high", imgs_)
                                                 click_pos_reg(imgs_.x, imgs_.y, cla)
@@ -267,7 +267,7 @@ def chango_ready(cla):
                                                 full_path = "c:\\my_games\\arthdal\\data_arthdal\\imgs\\chango\\box_list\\ganghwasuk_box_common.PNG"
                                                 img_array = np.fromfile(full_path, np.uint8)
                                                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                                                imgs_ = imgs_set_(720, 120, 950, 990, cla, img, 0.8)
+                                                imgs_ = imgs_set_(720, 120, 950, 990, cla, img, 0.7)
                                                 if imgs_ is not None and imgs_ != False:
                                                     print("ganghwasuk_box_common", imgs_)
                                                     click_pos_reg(imgs_.x, imgs_.y, cla)
